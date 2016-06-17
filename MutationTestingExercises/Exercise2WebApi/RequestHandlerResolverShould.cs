2cdaed4b3b12aa31921ca456b114dc998a3b04bc
Raphael Yoshiga <Raphael.Yoshiga@asos.com>
2016-06-17 11:01:12 +0100

    Add test coverage

--- MutationTestingExercises/Exercise2WebApi/RequestHandlerResolverShould.cs
+++ MutationTestingExercises/Exercise2WebApi/RequestHandlerResolverShould.cs
@@ -6,6 +6,8 @@ namespace MutationTestingExercises.Exercise2WebApi
     [TestFixture]
     public class RequestHandlerResolverShould
     {
+        private const string KITTENS_ROOT = "kittens/";
+        private const string KITTENS_RETRIEVE = "kittens/{0}";
         private RequestHandlerResolver _requestHandlerResolver;
 
         [SetUp]
@@ -17,26 +19,37 @@ namespace MutationTestingExercises.Exercise2WebApi
         [Test]
         public void Resolve_a_create_kitten_request()
         {
-            var requestHandler = _requestHandlerResolver.Resolve(HttpMethod.Post, "kittens/");
+            var requestHandler = _requestHandlerResolver.Resolve(HttpMethod.Post, KITTENS_ROOT);
 
             Assert.That(requestHandler, Is.TypeOf<CreateKittenRequestHandler>());
         }
 
-        [Test]
-        public void Resolve_a_retrieve_kitten_request()
+        [TestCase("Dog")]
+        [TestCase("Bob")]
+        [TestCase("fluffles")]
+        public void Resolve_a_retrieve_kitten_request(string kittenName)
         {
-            var requestHandler = _requestHandlerResolver.Resolve(HttpMethod.Get, "kittens/fluffles");
+            string kittenUrl = string.Format(KITTENS_RETRIEVE, kittenName);
+            var requestHandler = _requestHandlerResolver.Resolve(HttpMethod.Get, kittenUrl);
 
             Assert.That(requestHandler, Is.TypeOf<RetrieveKittenRequestHandler>());
         }
 
-        [Test]
-        public void Resolve_a_not_found_handler_for_an_unrecognised_request()
+        [TestCaseSource("UnrecognizedRequestsList")]
+        public void Resolve_a_not_found_handler_for_an_unrecognised_request(HttpMethod method, string relativePath)
         {
-            var requestHandler = _requestHandlerResolver.Resolve(HttpMethod.Put, "puppies/");
+            var requestHandler = _requestHandlerResolver.Resolve(method, relativePath);
 
             Assert.That(requestHandler, Is.TypeOf<NotFoundHandler>());
         }
+
+        static object[] UnrecognizedRequestsList =
+        {
+            new object[] { HttpMethod.Put, "puppies/"},
+            new object[] { HttpMethod.Get, KITTENS_ROOT },
+            new object[] { HttpMethod.Post, KITTENS_RETRIEVE }
+        };
+    
     }
 
 
